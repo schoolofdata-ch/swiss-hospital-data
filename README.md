b94af0d4d458a00bc454252c363e53e5f5b31f20
Oleg Lavrovsky <oleg@utou.ch>
2020-03-18 22:07:42 +0000

    Initial release :tada:

---
+++ README.md
@@ -0,0 +1,7 @@
+Swiss Hospitals Data
+---
+
+A #covid19mon hackathon project. 
+Details at https://db.schoolofdata.ch/project/68
+
+Publicly access at https://rstudio.cloud/project/1049226
---
+++ shc_data.R
@@ -0,0 +1,46 @@
+library(readxl)
+library(plyr)
+
+data <- read_excel("kzp17_daten.xlsx", sheet="KZ2017_KZP17")
+data <- data[!is.na(data$JAHR) & data$Inst!="Ganze Schweiz",]
+
+# Select vars to keep
+data <- data[,c("JAHR","KT","Inst","Adr","Ort","Typ","Akt","SL", "AnzStand", "SA", "LA", 
+                "Ops", "PersA", "PersP", "PersMT", "BettenStatA", "pBettenBelStatA", "KostStatA")]
+
+# Put Zip code in separate var
+data$PLZ <- substr(data$Ort,1,4)
+data$Ort <- sub("[0-9]{4} ","", data$Ort)
+
+# split multi string fields
+tosplit <-  c("Akt", "SL", "LA", "SA")
+
+for (i in tosplit) {
+    
+  foo <- strsplit(unlist(data[,i]),", ", fixed=T)
+  foo <- data.frame(do.call(rbind, lapply(lapply(foo, factor, levels=unique(unlist(foo))), table)))
+  colnames(foo) <- paste0(i, "_", colnames(foo))
+  data <- cbind(data, foo)
+  rm(foo)
+  
+}
+
+# Add typology
+typo <- read_excel("kzp17_daten.xlsx", sheet="Typologie")
+colnames(typo) <- c("Typ", "Typ_de", "Typ_fr", "Typ_it")
+
+data <- merge(data, typo, by="Typ", all=T)
+data <- cbind("id"=1:nrow(data), data)
+
+# check
+table(data$Akt, data$Akt_A)
+table(data$SL, data$SL_IPS)
+table(data$SA, data$SA_CT)
+
+# Remove multi string fields
+data <- data[,!(colnames(data) %in% tosplit)]
+
+# write
+write.csv(data, "shc_infra.csv", row.names = F, na="", fileEncoding = "UTF-8")
+
+
